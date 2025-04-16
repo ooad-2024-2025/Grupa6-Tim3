8c89a12f56264e5c8d686b6e6563b9b8afbe176f
Muhamed Dzafic <mdzafic1@etf.unsa.ba>
2025-04-16 17:15:41 +0200

    Implementacija

---
+++ Implementacija/RealEstateHub/RealEstateHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealEstateHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
