14d63cf3109b10bafb569205ee2be218d353be79
Muhamed Dzafic <mdzafic1@etf.unsa.ba>
2025-06-18 18:04:59 +0200

    Dokumentacija

--- Implementacija/RealEstateHub/RealEstateHub/obj/Release/net8.0/RealEstateHub.AssemblyInfo.cs
+++ Implementacija/RealEstateHub/RealEstateHub/obj/Release/net8.0/RealEstateHub.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c835184a3adac1bf07350a0eca2c283d348d6f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58947176d3779181a2342014574b217b456b2112")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
